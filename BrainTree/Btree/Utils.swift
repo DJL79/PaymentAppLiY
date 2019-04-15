637523705afa6a604e56a320cd12c3b9d4554539
TopStar323 <liyun0808@outlook.com>
2019-04-15 17:41:49 +0800

    Braintree iOS/Backend Tutorial

---
+++ BrainTree/Btree/Utils.swift
@@ -0,0 +1,75 @@
+//
+//  Utils.swift
+//  tasaquww
+//
+//  Created by TopStar on 2/15/18.
+//  Copyright © 2018 TopStar. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import NVActivityIndicatorView
+import UIKit
+import Toast_Swift
+
+class Utils{
+    
+    // set base url to call web service
+    static var base_url = "http://192.168.1.113/bt/"
+    
+    // strings
+    
+    static var str_err_server = "Something went wrong."
+    static var str_cancel = "You just canceled the checkout."
+    static var str_payment_success = "You payment was successful."
+    
+    // show progress dialog
+    static var activityData = ActivityData(size: nil,message: nil,messageFont: nil,messageSpacing: nil,type: .ballRotateChase,color: UIColor.init(red: 58/255.0, green: 64/255.0, blue: 140/255.0, alpha: 0.8),padding: nil,displayTimeThreshold: nil,minimumDisplayTime: nil,backgroundColor: UIColor(red: 1, green: 1, blue: 1, alpha: 0),textColor: UIColor.init(red: 58/255.0, green: 64/255.0, blue: 140/255.0, alpha: 0.8))
+    static func showProgress(){
+        NVActivityIndicatorPresenter.sharedInstance.startAnimating(self.activityData,nil)
+    }
+    static func hideProgress(){
+        NVActivityIndicatorPresenter.sharedInstance.stopAnimating(nil)
+    }
+    
+    // show toast
+    static func showToast(_ view:UIView, _ string:String){
+        view.makeToast(string)
+    }
+    
+    // show alert
+    static func showAlert(_ title:String, _ message:String, _ vc:UIViewController){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        vc.present(alert, animated: true, completion: nil)
+    }
+    
+    // send http request to server with alamofire
+    static func sendRequest(_ url : String , _ method : HTTPMethod ,_ parameters:[String:Any], completion: @escaping (Bool, Any) -> Void) {
+        Alamofire.request(base_url + url , method : method , parameters : parameters).validate().responseJSON { response in
+            print(response.value)
+            switch response.result {
+            case .success:
+                if let result = response.value as? [String:Any], let res = result["result"] as? Bool{
+                    completion(res, result["data"] as Any)
+                } else {
+                    completion(false,[:])
+                }
+            case .failure:
+                completion(false,[:])
+            }
+        }
+    }
+    
+    
+    // save/get value to/from Userdefault
+    static func setPreference(_ key : String, _ value : String){
+        UserDefaults.standard.set(value, forKey: key)
+    }
+    static func getPreference(_ key : String) -> String{
+        if let value = UserDefaults.standard.string(forKey: key) {
+            return value
+        }
+        return ""
+    }
+}
---
+++ BrainTree/Btree/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  braintree
+//
+//  Created by Dragon on 2019/4/10.
+//  Copyright © 2019 Dragon. All rights reserved.
+//
+
+import UIKit
+import BraintreeDropIn
+import Braintree
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func onPurchase(_ sender: Any) {
+        Utils.sendRequest("generate_client_token.php", .get, [:]) { (res, data) in
+            if res {
+                let clientToken = data as! String
+                let request =  BTDropInRequest()
+                request.applePayDisabled = false
+                request.cardDisabled = false
+                request.paypalDisabled = false
+                request.amount = "10"
+                let dropIn = BTDropInController(authorization: clientToken, request: request)
+                { (controller, result, error) in
+                    if (error != nil) {
+                        print(error)
+                        Utils.showToast(self.view, (error?.localizedDescription)!)
+                    } else if (result?.isCancelled == true) {
+                        Utils.showToast(self.view, Utils.str_cancel)
+                    } else if let result = result {
+                        Utils.showToast(self.view, Utils.str_payment_success)
+                    }
+                    controller.dismiss(animated: true, completion: nil)
+                }
+                self.present(dropIn!, animated: true, completion: nil)
+            } else {
+                Utils.showToast(self.view, data as? String ?? Utils.str_err_server)
+            }
+        }
+    }
+    
+}
+
